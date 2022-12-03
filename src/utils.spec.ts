7b064a27c224f10a4c6363e8cc6e69b3da2b7259
moonstar-x <christian.lopez99@outlook.com>
2022-12-03 04:25:00 -0500

    Created a getLocationForIp util.

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,4 +1,4 @@
-import { getPublicIp } from './utils';
+import { getPublicIp, getLocationForIp } from './utils';
 
 describe('Utils', () => {
   describe('getPublicIp()', () => {
@@ -13,6 +13,49 @@ describe('Utils', () => {
       jest.spyOn(global, 'fetch').mockResolvedValueOnce({ text: () => Promise.resolve('') } as any);
 
       expect(await getPublicIp()).toBe('');
+    });
+
+    it('should return empty if request rejects.', async () => {
+      jest.spyOn(global, 'fetch').mockRejectedValueOnce(new Error(''));
+
+      expect(await getPublicIp()).toBe('');
+    });
+  });
+
+  describe('getLocationForIp()', () => {
+    const ip = '123.123.123.123';
+
+    it('should return a location string if accessible.', async () => {
+      jest.spyOn(global, 'fetch').mockResolvedValueOnce(
+        {
+          json: () => {
+            return Promise.resolve({
+              success: true,
+              country: 'France',
+              city: 'Paris'
+            });
+          }
+        } as any);
+
+      expect(await getLocationForIp(ip)).toBe('Paris, France');
+    });
+
+    it('should return N/A string if not accessible.', async () => {
+      jest.spyOn(global, 'fetch').mockResolvedValueOnce(
+        {
+          json: () => {
+            return Promise.resolve({
+              success: false
+            });
+          }
+        } as any);
+      expect(await getLocationForIp(ip)).toBe('N/A');
+    });
+
+    it('should return N/A if request rejects.', async () => {
+      jest.spyOn(global, 'fetch').mockRejectedValueOnce(new Error(''));
+
+      expect(await getLocationForIp(ip)).toBe('N/A');
     });
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import 'cross-fetch/polyfill';
 
-const IP_API_URL = 'https://checkip.amazonaws.com/';
-// const GEO_API_URL = 'https://ip-api.com/docs/api:json';
+const IP_API_URL = 'https://checkip.amazonaws.com';
+const GEO_API_URL = 'http://ip-api.com/json';
 
 export const getPublicIp = async (): Promise<string> => {
   try {
@@ -9,5 +9,20 @@ export const getPublicIp = async (): Promise<string> => {
     return await response.text();
   } catch (error) {
     return '';
+  }
+};
+
+export const getLocationForIp = async (ip: string): Promise<string> => {
+  try {
+    const response = await fetch(`${GEO_API_URL}/${ip}`);
+    const json = await response.json();
+
+    if (!json.success) {
+      return 'N/A';
+    }
+
+    return `${json.city}, ${json.country}`;
+  } catch (error) {
+    return 'N/A';
   }
 };
